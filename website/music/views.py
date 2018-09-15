862b1493d0c8391067c7c1d56c85d378b9a7c517
Sagar Kumar <mads.silelan@gmail.com>
2018-09-15 13:37:00 +0530

    Added template folder and modify two files.

--- website/music/views.py
+++ website/music/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Album
+from django.template import loader
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<h1>HELLO APP")
+    all_albums = Album.objects.all()
+    template = loader.get_template('music/index.html')
+    context = {
+        'all_albums':all_albums,
+    }
+    return HttpResponse(template.render(context,request))
 
 def detail(request, album_id):
     return HttpResponse("<h2>Details for Album id: " + str(album_id) + "</h2>")
