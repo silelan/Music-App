40bf74956641b09ee6e8378ee3c37e69094d6a76
Sagar Kumar <mads.silelan@gmail.com>
2018-09-19 11:12:43 +0530

    replace to generic view

--- website/music/urls.py
+++ website/music/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'music'
+
 urlpatterns = [
-    path('', views.index, name = 'index'),
-    path('<int:album_id>/', views.detail,name = 'detail'),
+    path('', views.IndexView.as_view(), name = 'index'),
+    path('<int:pk>/', views.DetailView.as_view(),name = 'detail'),
 ]
--- website/music/views.py
+++ website/music/views.py
@@ -1,18 +1,14 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.views import generic
 from .models import Album
-from django.http import Http404
-
 
 # Create your views here.
-def index(request):
-    all_albums = Album.objects.all()
-    context = {'all_albums':all_albums}
-    return render(request, 'music/index.html',context)
+class IndexView(generic.ListView):
+    template_name = 'music/index.html'
+    context_object_name = 'all_albums'
 
-def detail(request, album_id):
-    try:
-        album = Album.objects.get(pk = album_id)
-    except Album.DoesNotExist:
-        raise Http404("Album Does Not Exits")
-    return render(request, 'music/detail.html', {'album':album})
+    def get_queryset(self):
+        return Album.objects.all()
+
+class DetailView(generic.DetailView):
+    model = Album
+    template_name = 'music/detail.html'
